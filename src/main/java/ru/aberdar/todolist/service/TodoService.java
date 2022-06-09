a53d50898dbc9d6eea7e26a1b625ae4bd656d3a4
aberdar <sashaberdar@icloud.com>
2022-06-09 10:23:06 +0300

    Create todo model

--- src/main/java/ru/aberdar/todolist/service/TodoService.java
+++ src/main/java/ru/aberdar/todolist/service/TodoService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.aberdar.todolist.entity.TodoEntity;
 import ru.aberdar.todolist.entity.UserEntity;
+import ru.aberdar.todolist.model.Todo;
 import ru.aberdar.todolist.repository.TodoRepository;
 import ru.aberdar.todolist.repository.UserRepository;
 
@@ -15,15 +16,15 @@ public class TodoService {
     @Autowired
     private UserRepository userRepository;
 
-    public TodoEntity createTodo(TodoEntity todo, Long userId) {
+    public Todo createTodo(TodoEntity todo, Long userId) {
         UserEntity user = userRepository.findById(userId).get();
         todo.setUser(user);
-        return todoRepository.save(todo);
+        return Todo.toModel(todoRepository.save(todo));
     }
 
-    public TodoEntity completeTodo(Long id) {
+    public Todo completeTodo(Long id) {
         TodoEntity todo = todoRepository.findById(id).get();
         todo.setCompleted(!todo.getCompleted());
-        return todoRepository.save(todo);
+        return Todo.toModel(todoRepository.save(todo));
     }
 }
